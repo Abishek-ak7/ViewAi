d580f42d439f5a82af535d7b6d5af2e127d78f6b
Koushik <koushikn0419@gmail.com>
2024-06-19 22:32:54 +0530

    connect with us section done

---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Home from './Home'
+import About from './About'
+import ConnectWithUs from './ConnectWithUs'
+
+export default function Main() {
+  return (
+    <div>
+        <Home />
+        <About/>
+        <ConnectWithUs/>
+        
+    </div>
+  )
+}
