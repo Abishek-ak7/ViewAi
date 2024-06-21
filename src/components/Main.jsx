affa74451ade3290ab9fad8851a3acd292ec8c0a
Ashiq <ashiqurrahman2205@gmail.com>
2024-06-21 11:58:22 +0530

    bg done for about & connectwu

---
+++ src/components/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Home from './Home'
+import About from './About'
+import ConnectWithUs from './ConnectWithUs'
+
+export default function Main() {
+  return (
+    <div>
+        <Home />
+        <About/>
+
+        
+       
+    </div>
+  )
+}
